be91cad2f2084302f0d2551fff0b9d46abb9db21
Jordan Quinn <jordan.quinn@alteryx.com>
2020-10-14 09:43:31 -0600

    Add appheader/toggles

---
+++ src/AppHeader/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { AppBar, Grid, Typography, Button } from '@ayx/ui-core';
+
+interface IAppHeaderProps {
+  toggleModelDrawer: any;
+}
+
+const AppHeader = (props: IAppHeaderProps): JSX.Element => {
+  const { toggleModelDrawer } = props;
+
+  return (
+    <AppBar color="primary" position="static">
+      <Grid container>
+        <Grid item xs={4}>
+          <Typography variant="h1">UI-SDK Dev Harness</Typography>
+        </Grid>
+      </Grid>
+      <Grid container>
+        <Grid item xs={4}>
+          <Button color="primary" onClick={toggleModelDrawer} variant="contained">
+            Show Model
+          </Button>
+        </Grid>
+      </Grid>
+    </AppBar>
+  );
+};
+
+export default AppHeader;
